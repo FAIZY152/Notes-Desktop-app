b1293b7074202402f1aea8da723bf17529686b83
Dev2-marketasa <dev2@marketasa.com>
2026-06-16 22:11:34 +0500

    add-main-window-page

--- noteforge/src/electron/main.ts
+++ noteforge/src/electron/main.ts
@@ -20,7 +20,8 @@ function createWindow(): void {
 
   mainWindow.on('ready-to-show', () => {
     mainWindow.show()
-  })}
+  })
+}
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
